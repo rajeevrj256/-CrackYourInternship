28a611f070ec738fcc2c917c03eb1cae63497785
rajeevrj256 <rjrajeev5918@gmail.com>
2024-07-17 02:37:23 +0530

    spiral matrix

---
+++ Day2/Spiral_Matrix.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int>ans;
+        int startrow=0;
+        int startcol=0;
+
+        int endrow=matrix.size()-1;
+        int endcol=matrix[0].size()-1;
+        while(startrow<=endrow && startcol<=endcol){
+            for(int i=startcol;i<=endcol;i++){
+                ans.push_back(matrix[startrow][i]);
+            }
+            startrow++;
+
+            for(int i=startrow;i<=endrow;i++){
+                ans.push_back(matrix[i][endcol]);
+            }
+            endcol--;
+             if((startrow>endrow) || (startcol>endcol)){
+                break;
+            }
+
+            for(int i=endcol;i>=startcol;i--){
+                ans.push_back(matrix[endrow][i]);
+            }
+            endrow--;
+
+            for(int i=endrow;i>=startrow;i--){
+                ans.push_back(matrix[i][startcol]);
+            }
+            startcol++;
+
+            
+
+
+        }
+        return ans;
+    }
+};
