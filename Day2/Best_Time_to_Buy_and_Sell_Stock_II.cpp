cca56068031ac378b43963b51a0fb6377f995f71
rajeevrj256 <rjrajeev5918@gmail.com>
2024-07-17 00:03:10 +0530

    best2

---
+++ Day2/Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<int>dp(n,0);
+        for(int i=1;i<n;i++){
+            if(prices[i]>prices[i-1]){
+                dp[i]=dp[i-1]+(prices[i]-prices[i-1]);
+            }
+            else{
+                dp[i]=dp[i-1];
+            }
+        }
+        return dp[n-1];
+
+
+
+
+    }
+};
