5dfbbd22b10097bd0c448fa66b60dbf58ba99de8
rajeevrj256 <rjrajeev5918@gmail.com>
2024-07-17 13:38:33 +0530

    majority element

---
+++ majority_element.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map <int,int>hash;
+        for(int i:nums){
+            hash[i]++;
+            if(hash[i]>nums.size()/2)return i;
+        }
+        return 0;
+    }
+};
