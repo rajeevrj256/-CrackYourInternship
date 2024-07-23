b9e28a690d9565cbc8646c9152b487faf1023446
rajeevrj256 <rjrajeev5918@gmail.com>
2024-07-23 22:06:27 +0530

    excel sheet column title

---
+++ Excel_Sheet_Column_Title.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string ans;
+        while(columnNumber--){
+            ans.push_back(columnNumber%26+'A');
+            columnNumber=columnNumber/26;
+        }
+        reverse(ans.begin(),ans.end());
+
+        return ans;
+    }
+};
