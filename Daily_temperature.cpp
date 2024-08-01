0649dcedf9262a8cd0b04b4744fbe8f67868a808
rajeevrj256 <rjrajeev5918@gmail.com>
2024-08-01 23:53:26 +0530

    Daily temperature

---
+++ Daily_temperature.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <stack>
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<int> s; 
+
+        for (int i = 0; i < n; ++i) {
+            while (!s.empty() && temperatures[i] > temperatures[s.top()]) {
+                int prevIndex = s.top();
+                s.pop();
+                ans[prevIndex] = i - prevIndex;
+            }
+            s.push(i);
+        }
+
+        return ans;
+    }
+};
