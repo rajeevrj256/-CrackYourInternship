077fd8a260a524e31fb5e9c2d156bb51d35256e9
rajeevrj256 <rjrajeev5918@gmail.com>
2024-08-20 17:01:21 +0530

    Remove All Adjacent Duplicates in String II

---
+++ Remove All Adjacent Duplicates in String II.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string removeDuplicates(string str, int k) {
+        stack<pair<char,int>>s;
+        for(char c:str){
+            if(!s.empty() && s.top().first==c){
+                s.top().second++;
+            }else{
+                s.push({c,1});
+            }
+
+            if(s.top().second==k){
+                s.pop();
+
+            }
+        }
+        string res="";
+        while(!s.empty()){
+            for(int i=0;i<s.top().second;i++){
+
+                 res+=s.top().first;
+            }
+            s.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
