6fcec67c6b5f8cc7dd25859e1b8fc29c8fe60106
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2024-12-19 15:29:17 +0530

    Time: 2 ms (82.38%) | Memory: 15.7 MB (29.86%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    void backtracking(string&str,vector<string>&res,int open,int close,int n){
+    void backtracking(string str,vector<string>&res,int open,int close,int n){
          if(open==n && close==n){
             res.push_back(str);
             return ;
          }
 
          if(open<n){
-            str.push_back('(');
-            backtracking(str,res,open+1,close,n);
-            str.pop_back();
+            
+            backtracking(str+"(",res,open+1,close,n);
+           
          }
 
          if(close<open){
-            str.push_back(')');
-            backtracking(str,res,open,close+1,n);
-            str.pop_back();
+           
+            backtracking(str+")",res,open,close+1,n);
+            
          }
 
         
