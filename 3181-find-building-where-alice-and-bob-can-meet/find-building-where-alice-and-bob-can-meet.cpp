b5c5af3feecda1d8c3104c8b2d03eb4071893b98
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2024-12-22 17:21:34 +0530

    Time: 249 ms (57.96%) | Memory: 321.3 MB (18.47%) - LeetSync

--- 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
+++ 3181-find-building-where-alice-and-bob-can-meet/find-building-where-alice-and-bob-can-meet.cpp
@@ -2,46 +2,39 @@ class Solution {
 public:
     vector<int> leftmostBuildingQueries(vector<int>& heights,
                                         vector<vector<int>>& queries) {
-        vector<pair<int, int>> monoStack;
+        vector<vector<vector<int>>> storeQueries(heights.size());
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>
+            maxIndex;
         vector<int> result(queries.size(), -1);
-        vector<vector<pair<int, int>>> newQueries(heights.size());
-        for (int i = 0; i < queries.size(); i++) {
-            int a = queries[i][0];
-            int b = queries[i][1];
-            if (a > b) swap(a, b);
-            if (heights[b] > heights[a] || a == b)
-                result[i] = b;
-            else
-                newQueries[b].push_back({heights[a], i});
+
+        // Store the mappings for all queries in storeQueries.
+        for (int currQuery = 0; currQuery < queries.size(); currQuery++) {
+            int a = queries[currQuery][0], b = queries[currQuery][1];
+            if (a < b && heights[a] < heights[b]) {
+                result[currQuery] = b;
+            } else if (a > b && heights[a] > heights[b]) {
+                result[currQuery] = a;
+            } else if (a == b) {
+                result[currQuery] = a;
+            } else {
+                storeQueries[max(a, b)].push_back(
+                    {max(heights[a], heights[b]), currQuery});
+            }
         }
 
-        for (int i = heights.size() - 1; i >= 0; i--) {
-            int monoStackSize = monoStack.size();
-            for (auto& [a, b] : newQueries[i]) {
-                int position = search(a, monoStack);
-                if (position < monoStackSize && position >= 0)
-                    result[b] = monoStack[position].second;
+        for (int index = 0; index < heights.size(); index++) {
+            // If the priority queue's minimum pair value is less than the
+            // current index of height, it is an answer to the query.
+            while (!maxIndex.empty() && maxIndex.top()[0] < heights[index]) {
+                result[maxIndex.top()[1]] = index;
+                maxIndex.pop();
             }
-            while (!monoStack.empty() && monoStack.back().first <= heights[i])
-                monoStack.pop_back();
-            monoStack.push_back({heights[i], i});
+            // Push the with their maximum index as the current index in the
+            // priority queue.
+            for (auto& element : storeQueries[index]) {
+                maxIndex.push(element);
+            }
         }
         return result;
-    }
-
-private:
-    int search(int height, vector<pair<int, int>>& monoStack) {
-        int left = 0;
-        int right = monoStack.size() - 1;
-        int ans = -1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (monoStack[mid].first > height) {
-                ans = max(ans, mid);
-                left = mid + 1;
-            } else
-                right = mid - 1;
-        }
-        return ans;
     }
 };
