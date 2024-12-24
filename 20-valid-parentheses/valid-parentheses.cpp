f5d3c77d57655d6a0e1f9b73283a60efb58a7652
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2024-12-24 20:30:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (34.61%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(char c:s){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            }else{
+                if( st.empty() || ((c==')' && st.top()!='(') || (c=='}' && st.top()!='{') || (c==']' && st.top()!='[')) ){
+                    return false;
+                }else{
+
+                 st.pop();
+                }
+            }
+        }
+
+        return st.empty();
+    }
+};
