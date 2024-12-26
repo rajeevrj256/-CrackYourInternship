4089ad0c8411ccee9f00a6d9e6f3adbb8e828007
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2024-12-26 16:47:43 +0530

    Time: 7 ms (53.43%) | Memory: 15.9 MB (15.00%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,109 +1,100 @@
 class Solution {
 public:
-    long long mod = 1e9 + 7;
-    
-    // Function to get next smaller left index
-    vector<int> getnsl(vector<int>& arr, int n) {
-        vector<int> res;
-        stack<int> st;
-        for (int i = 0; i < n; i++) {
-            // Find the index of the first element smaller than arr[i]
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
+
+    vector<int>nextSmallerLeft(vector<int>&nums,int n){
+        stack<int>st;
+        vector<int>res(n);
+        for(int i=0;i<n;i++){
+              while(!st.empty() && nums[st.top()]>=nums[i]){
+                st.pop();
+              }
+
+              res[i]=(st.empty()?-1:st.top());
+              st.push(i);
+        }
+
+        return res;
+
+    }
+
+    vector<int>nextSmallerRight(vector<int>&nums,int n){
+        stack<int>st;
+        vector<int>res(n);
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()]>nums[i]){
                 st.pop();
             }
-            // If the stack is empty, there is no smaller element on the left, set to -1
-            res.push_back(st.empty() ? -1 : st.top());
+            res[i]=(st.empty()?n:st.top());
             st.push(i);
         }
+
         return res;
+
     }
-    
-    // Function to get next smaller right index
-    vector<int> getnsr(vector<int>& arr, int n) {
-        vector<int> res(n);
-        stack<int> st;
-        for (int i = n - 1; i >= 0; i--) {
-            // Find the index of the first element smaller than arr[i] from the right
-            while (!st.empty() && arr[st.top()] > arr[i]) {
+
+    vector<int>nextLargestLeft(vector<int>&nums,int n){
+        stack<int>st;
+        vector<int>res(n);
+        for(int i=0;i<n;i++){
+              while(!st.empty() && nums[st.top()]<=nums[i]){
                 st.pop();
-            }
-            // If the stack is empty, there is no smaller element on the right, set to n (out of bounds)
-            res[i] = (st.empty() ? n : st.top());
-            st.push(i);
+              }
+
+              res[i]=(st.empty()?-1:st.top());
+              st.push(i);
         }
+
         return res;
-    }
-    
-    // Function to calculate the sum of subarray minimums
-    long long  sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> nsl = getnsl(arr, n);
-        vector<int> nsr = getnsr(arr, n);
-        long long ans = 0;
 
-        for (int i = 0; i < n; i++) {
-            long long nslno = i - nsl[i];
-            long long nsrno = nsr[i] - i;
-            long long ways = nslno * nsrno;
-            long long totalsum = (arr[i] * ways) ;
-            ans = (ans + totalsum) ;
-        }
-
-        return ans;
     }
 
-    // Function to get next larger left index
-    vector<int> getnll(vector<int>& arr, int n) {
-        vector<int> res;
-        stack<int> st;
-        for (int i = 0; i < n; i++) {
-            while (!st.empty() && arr[st.top()] <= arr[i]) {
+    vector<int>nextLargestRight(vector<int>&nums,int n){
+        stack<int>st;
+        vector<int>res(n);
+        for(int i=n-1;i>=0;i--){
+              while(!st.empty() && nums[st.top()]<nums[i]){
                 st.pop();
-            }
-            res.push_back(st.empty() ? -1 : st.top());
-            st.push(i);
+              }
+              res[i]=(st.empty()?n:st.top());
+              st.push(i);
         }
+
         return res;
+
     }
+
+
     
-    // Function to get next larger right index
-    vector<int> getnlr(vector<int>& arr, int n) {
-        vector<int> res(n);
-        stack<int> st;
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && arr[st.top()] < arr[i]) {
-                st.pop();
-            }
-            res[i] = (st.empty() ? n : st.top());
-            st.push(i);
-        }
-        return res;
-    }
+
+
+
     
-    // Function to calculate the sum of subarray maximums
-    long long sumSubarrayMax(vector<int>& arr) {
-        int n = arr.size();
-        vector<int> nsl = getnll(arr, n);
-        vector<int> nsr = getnlr(arr, n);
-        long long ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            long long nslno = i - nsl[i];
-            long long nsrno = nsr[i] - i;
-            long long ways = nslno * nsrno;
-            long long totalsum = (arr[i] * ways);
-            ans = (ans + totalsum) ;
-        }
-
-        return ans;
-    }
-
-    // Final function to calculate subarray ranges
     long long subArrayRanges(vector<int>& nums) {
-        long long maxSum = sumSubarrayMax(nums);
-        long long minSum = sumSubarrayMins(nums);
+         int n=nums.size();
+        vector<int>nsl=nextSmallerLeft(nums,n);
+        vector<int>nsr=nextSmallerRight(nums,n);
+        
+        vector<int>nll=nextLargestLeft(nums,n);
+        vector<int>nlr=nextLargestRight(nums,n);
 
-        // The result should be the difference of the maximum sum and the minimum sum
-        return (maxSum - minSum ) ;
+
+        long long mintotal=0;
+        long long maxtotal=0;
+
+        for(int i=0;i<n;i++){
+            long long left=i-nsl[i];
+            long long right=nsr[i]-i;
+            long long totalways=(left*right);
+            mintotal+=(totalways*nums[i]);
+
+        }
+        for(int i=0;i<n;i++){
+            long long left=i-nll[i];
+            long long right=nlr[i]-i;
+            long long totalways=(left*right);
+            maxtotal+=(totalways*nums[i]);
+            
+        }
+        return maxtotal-mintotal;
     }
 };
