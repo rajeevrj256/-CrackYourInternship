e994148dcabb1e0950c22b358f178fe2dade7b92
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2024-12-31 15:29:56 +0530

    Time: 45 ms (37.69%) | Memory: 32.2 MB (66.41%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
-    bool helper(vector<int>&hand,int groupsize,int start,int end){
-        int nextele=hand[start]+1;
-        hand[start]=-1;
-        int i=start+1;
-        int grpcount=1;
+    // bool helper(vector<int>&hand,int groupsize,int start,int end){
+    //     int nextele=hand[start]+1;
+    //     hand[start]=-1;
+    //     int i=start+1;
+    //     int grpcount=1;
         
-        while(i<end && grpcount<groupsize){
-            if(hand[i]==nextele){
-                nextele=hand[i]+1;
+    //     while(i<end && grpcount<groupsize){
+    //         if(hand[i]==nextele){
+    //             nextele=hand[i]+1;
                 
-                hand[i]=-1;
-                grpcount++;
-            }
-            i++;
+    //             hand[i]=-1;
+    //             grpcount++;
+    //         }
+    //         i++;
 
-        }
-        return (grpcount==groupsize)?true:false;
+    //     }
+    //     return (grpcount==groupsize)?true:false;
 
         
-    }
+    // }
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         if(hand.size()%groupSize!=0)return false;
-        sort(hand.begin(),hand.end());
+         map<int,int>minheap;
 
-        for(int i=0;i<hand.size();i++){
-            if(hand[i]>=0){
-                if(!helper(hand,groupSize,i,hand.size())){
+         for(int i=0;i<hand.size();i++){
+            minheap[hand[i]]++;
+         }
+
+         while(!minheap.empty()){
+            int currCard=minheap.begin()->first;
+            for(int i=0;i<groupSize;i++){
+                if(minheap[currCard+i]==0){
                     return false;
                 }
+                minheap[currCard+i]--;
+                if(minheap[currCard+i]<1){
+                    minheap.erase(currCard+i);
+                }
             }
-        }
+         }
         return true;
 
         
