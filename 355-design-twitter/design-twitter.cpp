73e05822520012499da85269aa7c600ef5928152
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2024-12-31 17:02:39 +0530

    Time: 815 ms (5.62%) | Memory: 56.2 MB (24.44%) - LeetSync

--- 355-design-twitter/design-twitter.cpp
+++ 355-design-twitter/design-twitter.cpp
@@ -1,42 +1,32 @@
 class Twitter {
 public:
-    unordered_set<int>user[501];
-    vector<pair<int,int>>tweet;
-    Twitter() {
-        
-    }
-    
+    unordered_set<int> user[501];
+    vector<pair<int,int>> tweet;
+
+    Twitter() {}
+
     void postTweet(int userId, int tweetId) {
-        tweet.push_back({tweetId,userId});
-        
+        tweet.push_back({tweetId, userId});
     }
-    
+
     vector<int> getNewsFeed(int userId) {
-        int t=tweet.size();
-        vector<int>ans;
-        for(int i=t-1;i>=0 && ans.size()<10;i--){
-            if(tweet[i].second==userId || user[userId].find(tweet[i].second)!=user[userId].end()){
+        int t = tweet.size();
+        vector<int> ans;
+        for(int i = t - 1; i >= 0 && ans.size() < 10; i--) {
+            if(tweet[i].second == userId || user[userId].find(tweet[i].second) != user[userId].end()) {
                 ans.push_back(tweet[i].first);
             }
         }
         return ans;
     }
-    
+
     void follow(int followerId, int followeeId) {
         user[followerId].insert(followeeId);
-        
     }
-    
+
     void unfollow(int followerId, int followeeId) {
-        user[followerId].erase(followeeId);
+        if(user[followerId].find(followeeId) != user[followerId].end()) {
+            user[followerId].erase(followeeId);
+        }
     }
 };
-
-/**
- * Your Twitter object will be instantiated and called as such:
- * Twitter* obj = new Twitter();
- * obj->postTweet(userId,tweetId);
- * vector<int> param_2 = obj->getNewsFeed(userId);
- * obj->follow(followerId,followeeId);
- * obj->unfollow(followerId,followeeId);
- */
