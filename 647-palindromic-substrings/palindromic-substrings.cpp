632617058d7ec9b35be6579ee1da6ab22ca68f54
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-01-01 22:54:23 +0530

    Time: 4 ms (76.04%) | Memory: 8.6 MB (73.35%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,30 +1,25 @@
-class Solution {
-public:
-    int t[1001][1001];
-    
-    bool ispalindrome(string s, int i, int j) {
-        if (i >= j) return true;  // Single character or empty substring is a palindrome
-        
-        if (t[i][j] != -1) return t[i][j];
-        
-        if (s[i] == s[j]) {
-            return t[i][j] = ispalindrome(s, i + 1, j - 1);
+class Solution 
+{
+private:
+    string t;
+    int check(int l, int r, int ans = 0)
+    {
+        while(l >= 0 and r <= t.size())
+        {
+            if(t[l--] == t[r++]) ans++;
+            else break;
         }
-        
-        return t[i][j] = false;  // If characters don't match, it's not a palindrome
+        return ans;
     }
-    
-    int countSubstrings(string s) {
-        int n = s.size();
-        int cnt = 0;
-        memset(t, -1, sizeof(t));  // Initialize memoization table
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {  // Generate all substrings s[i..j]
-                if (ispalindrome(s, i, j)) cnt++;
-            }
+public:
+    int countSubstrings(string s) 
+    {
+        int n = size(s), ans = 0;t = s;
+        for(int i = 0; i < n; i++)
+        {
+            ans += check(i,i);    // odd length palindromes
+            ans += check(i,i+1); // even length palindromes
         }
-        
-        return cnt;
+        return ans;
     }
 };
