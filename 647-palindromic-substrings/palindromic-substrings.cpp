7db187fde610bfdedc5a8b4e05c458c22b91fb62
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-01-01 22:22:37 +0530

    Time: 1872 ms (5.00%) | Memory: 418.4 MB (7.55%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-     int t[1001][1001];
-     bool ispalindrome(string s,int i,int j){
-        if(i>=j)return true;
-        if(t[i][j]!=-1)return t[i][j];
-
-        if(s[i]==s[j]){
-           return  t[i][j]=ispalindrome(s,i+1,j-1);
-        }
-
-        return t[i][j]=false;
-     }
-     
-    int countSubstrings(string s) {
+    int t[1001][1001];
+    
+    bool ispalindrome(string s, int i, int j) {
+        if (i >= j) return true;  // Single character or empty substring is a palindrome
         
-        int n=s.size();
-        int cnt=0;
-        memset(t,-1,sizeof(t));
-       for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            
-            if(ispalindrome(s,i,j))cnt++;
+        if (t[i][j] != -1) return t[i][j];
+        
+        if (s[i] == s[j]) {
+            return t[i][j] = ispalindrome(s, i + 1, j - 1);
         }
-       }
-            
+        
+        return t[i][j] = false;  // If characters don't match, it's not a palindrome
+    }
+    
+    int countSubstrings(string s) {
+        int n = s.size();
+        int cnt = 0;
+        memset(t, -1, sizeof(t));  // Initialize memoization table
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {  // Generate all substrings s[i..j]
+                if (ispalindrome(s, i, j)) cnt++;
+            }
+        }
+        
         return cnt;
     }
 };
