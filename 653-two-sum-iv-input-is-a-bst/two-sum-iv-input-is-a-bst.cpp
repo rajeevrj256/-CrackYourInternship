cfbd07ea7e9eb8e68d4acf5d7f13f2b020e696ad
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-01-15 15:55:31 +0530

    Time: 0 ms (100.00%) | Memory: 37.6 MB (34.64%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -1,40 +1,33 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-void inorder(TreeNode* root, vector<int> &ans){
-    if(root == nullptr) return;
-    inorder(root->left, ans);
-    ans.push_back(root->val);
-    inorder(root->right, ans);
-}
 public:
     bool findTarget(TreeNode* root, int k) {
-        vector<int> ans;
-        inorder(root, ans);
+        vector<int> nums;
+        inorder(root, nums);
+        return findTargetInSortedArray(nums, k);
+    }
 
-        int start = 0;
+private:
+    void inorder(TreeNode* node, vector<int>& nums) {
+        if (!node) return;
+        inorder(node->left, nums);
+        nums.push_back(node->val);
+        inorder(node->right, nums);
+    }
 
-        int end = ans.size() - 1;
-        int sum = -1;
-        while(start < end){
-            sum = ans[start] + ans[end];
-            if(sum == k){
+    bool findTargetInSortedArray(vector<int> a, int target) {
+        for (int i = 0, j = a.size() - 1; i < j;) {
+            int sum = a[i] + a[j];
+            if (sum == target) {
                 return true;
-            }else if(sum > k){
-                end--;
-            }else{
-                start++;
+            }
+            else if (sum < target) {
+                i++;
+            }
+            else {
+                j--;
             }
         }
+
         return false;
     }
 };
