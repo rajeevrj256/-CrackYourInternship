1399ab4018c5592aac400c92436a743353eb0455
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-01-19 23:14:42 +0530

    Time: 113 ms (18.34%) | Memory: 121.3 MB (34.59%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/map-of-highest-peak.cpp
@@ -1,51 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> highestPeak(vector<vector<int>>& grid) {
-        int n = grid.size(); 
-	    int m = grid[0].size(); 
-	    // visited and distance matrix
-	    vector<vector<int>> vis(n, vector<int>(m, 0)); 
-	    vector<vector<int>> dist(n, vector<int>(m, 0)); 
-	    // <coordinates, steps>
-	    queue<pair<pair<int,int>, int>> q; 
-	    // traverse the matrix
-	    for(int i = 0;i<n;i++) {
-	        for(int j = 0;j<m;j++) {
-	            // start BFS if cell contains 1
-	            if(grid[i][j] == 1) {
-	                q.push({{i,j}, 0}); 
-	                vis[i][j] = 1; 
-	            }
-	            else {
-	                // mark unvisited 
-	                vis[i][j] = 0; 
-	            }
-	        }
-	    }
-	    
-	    int delrow[] = {-1, 0, +1, 0}; 
-	    int delcol[] = {0, +1, 0, -1}; 
-	    
-	    // traverse till queue becomes empty
-	    while(!q.empty()) {
-	        int row = q.front().first.first; 
-	        int col = q.front().first.second; 
-	        int steps = q.front().second; 
-	        q.pop(); 
-	        dist[row][col] = steps; 
-	        // for all 4 neighbours
-	        for(int i = 0;i<4;i++) {
-	            int nrow = row + delrow[i]; 
-	            int ncol = col + delcol[i]; 
-	            // check for valid unvisited cell
-	            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m 
-	            && vis[nrow][ncol] == 0) {
-	                vis[nrow][ncol] = 1; 
-	                q.push({{nrow, ncol}, steps+1});  
-	            }
-	        }
-	    }
-	    // return distance matrix
-	    return dist; 
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        vector<vector<int>>res(n,vector<int>(m,0));
+
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    q.push({{i,j},0});
+                    vis[i][j]=true;
+                }
+            }
+        }
+        int dx[4]={-1, 0, +1, 0}; 
+        int dy[4]= {0, +1, 0, -1}; 
+
+        while(!q.empty()){
+            int x=q.front().first.first;
+            int y=q.front().first.second;
+            int len=q.front().second;
+            q.pop();
+            res[x][y]=len;
+
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+
+                if(nx>=0 && ny>=0 && nx<n && ny<m && grid[nx][ny]==0 && !vis[nx][ny]){
+
+                    q.push({{nx,ny},len+1});
+                    vis[nx][ny]=true;
+                }
+            }
+        }
+        return res;
     }
 };
