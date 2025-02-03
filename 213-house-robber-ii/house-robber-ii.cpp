7bfea2952c36c1ee48ac167aba26bfdac36162e7
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-02-03 23:53:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (21.91%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     
-    int help(vector<int>&nums,int index,vector<int>&t){
-        if(index==0)return nums[0];
-        if(index<0)return 0;
-        if(t[index]!=-1)return t[index];
-        int rob=nums[index]+help(nums,index-2,t);
-        int notrob=0+help(nums,index-1,t);
-
-        return t[index]=max(rob,notrob);
+    int help(vector<int>&nums){
+        int prev=nums[0];
+        int prev2=0;
+        int n=nums.size();
+        for(int i=1; i<n; i++){
+          int pick = nums[i];
+        if(i>1)
+            pick += prev2;
+        int  nonPick = 0 + prev;
+        
+         int cur_i = max(pick, nonPick);
+        prev2 = prev;
+        prev= cur_i;
+        
+    }
+    return prev;
     }
     int rob(vector<int>& nums) {
 
@@ -16,12 +24,10 @@ public:
         vector<int> arr1(nums.begin(), nums.end() - 1);  // Exclude last house
         vector<int> arr2(nums.begin() + 1, nums.end());  // Exclude first house
 
-        // DP arrays for both cases
-        vector<int> dp1(arr1.size(), -1);
-        vector<int> dp2(arr2.size(), -1);
+        
 
-        int ans1=help(arr1,arr1.size()-1,dp1);
-        int ans2=help(arr2,arr2.size()-1,dp2);
+        int ans1=help(arr1);
+        int ans2=help(arr2);
 
         return max(ans1,ans2);
     }
