59841cdbb47386f3cd1786341edd2ea9e7c8a79b
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-02-04 14:08:31 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (27.97%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -13,7 +13,20 @@ public:
     }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-                vector<vector<int>>dp(n,vector<int>(n,-1));
-        return help(triangle,dp,0,0,n);
+            vector<vector<int>>dp(n,vector<int>(n,-1));
+
+            for(int i=0;i<n;i++){
+                dp[n-1][i]=triangle[n-1][i];
+            }
+            for(int i=n-2;i>=0;i--){
+                for(int j=i;j>=0;j--){
+                    int down=dp[i+1][j]+triangle[i][j];;
+                    int down_diagonal=dp[i+1][j+1]+triangle[i][j];
+
+                    dp[i][j]=min(down,down_diagonal);
+
+                }
+            }
+        return dp[0][0];
     }
 };
