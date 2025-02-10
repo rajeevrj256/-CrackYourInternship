819150fc5fe06b819cdc0c9837c653d5d90cf93b
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-02-10 22:47:06 +0530

    Time: 25 ms (42.79%) | Memory: 45.3 MB (20.23%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    int solve(vector<int>&coins,vector<vector<int>>&dp,int index,int target){
-        if(index==0){
-            if(target==0) return 1;
-            if(target%coins[0]==0) return 1;
-
-            return 0;
-        }
-
-        if(dp[index][target]!=-1)return dp[index][target];
-
-        int skip=solve(coins,dp,index-1,target);
-
-        int take=0;
-        if(coins[index]<=target){
-            take=solve(coins,dp,index,target-coins[index]);
-        }
-
-        return dp[index][target]=take+skip;
-    }
     int change(int amount, vector<int>& coins) {
-        int totalSum=accumulate(coins.begin(),coins.end(),0);
-        int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return solve(coins,dp,n-1,amount);
+        int n = coins.size();
+        vector<long long>v(amount+1);
+        for(int i=0;i<=amount;i++){
+            if(i % coins[0] == 0){
+                v[i] = 1; //i / coins[0];
+            }
+        }
+        for(int i=1;i<n;i++){
+            vector<long long>curr(amount+1);
+            for(int j=0;j<=amount;j++){
+                int pick = 0;
+                if(coins[i] <= j){
+                    pick = curr[j-coins[i]];
+                }
+                int no_pick = v[j];
+                curr[j] = (long)pick+no_pick;
+            }
+            v = curr;
+        }
+        return v[amount];
     }
 };
