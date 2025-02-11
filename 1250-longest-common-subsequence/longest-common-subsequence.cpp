5ef3f094983cf9812a06d41d0278f5afd80775c7
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-02-11 18:07:45 +0530

    Time: 23 ms (78.66%) | Memory: 26.8 MB (77.00%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -18,23 +18,20 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        for(int i=0;i<=n;i++){
-          dp[i][0]=0;
-        }
-        for(int i=0;i<=m;i++){
-          dp[0][i]=0;
-        }
+        vector<int>prev(m+1,0);
+        
         for(int i=1;i<=n;i++){
+            vector<int>curr(m+1,0);
             for(int j=1;j<=m;j++){
                  if(text1[i-1]==text2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+                    curr[j]=1+prev[j-1];
                  }else{
-                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                    curr[j]=max(curr[j-1],prev[j]);
                  }
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
 
         //return help(text1,0,text2,0,dp);
         
