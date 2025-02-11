d9888b4cb453fd1182fc49cc5b5179e66870ab4d
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-02-11 19:53:23 +0530

    Time: 83 ms (48.57%) | Memory: 75 MB (77.98%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -5,19 +5,21 @@ public:
         reverse(t.begin(),t.end());
         int n=s.size();
 
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+     vector<int>dp(n+1,0);
 
         for(int i=1;i<=n;i++){
+            vector<int>curr(n+1,0);
             for(int j=1;j<=n;j++){
                 if(s[i-1]==t[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+                    curr[j]=1+dp[j-1];
                 }else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                    curr[j]=max(dp[j],curr[j-1]);
                 }
             }
+            dp=curr;
         }
         
 
-        return dp[n][n];
+        return dp[n];
     }
 };
