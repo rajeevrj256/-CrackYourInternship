aba555e6b9d754195d0ca0267e22ffa53d54f54c
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-02-13 19:29:11 +0530

    Time: 280 ms (81.09%) | Memory: 17.9 MB (96.98%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,38 +1,57 @@
 class Solution:
-    def solve(self,s:str,i:int,t:str,j:int,dp:list[list[int]]) ->int:
-        if j<0:
-            return 1
-        if i<0:
-            return 0
+    # def solve(self,s:str,i:int,t:str,j:int,dp:list[list[int]]) ->int:
+    #     if j<0:
+    #         return 1
+    #     if i<0:
+    #         return 0
 
-        if dp[i][j]!=-1:
-            return dp[i][j]
+    #     if dp[i][j]!=-1:
+    #         return dp[i][j]
 
-        res=0
-        if s[i]==t[j]:
-            res=self.solve(s,i-1,t,j-1,dp)+self.solve(s,i-1,t,j,dp)
-        else:
-            res=self.solve(s,i-1,t,j,dp)
+    #     res=0
+    #     if s[i]==t[j]:
+    #         res=self.solve(s,i-1,t,j-1,dp)+self.solve(s,i-1,t,j,dp)
+    #     else:
+    #         res=self.solve(s,i-1,t,j,dp)
 
-        dp[i][j]=res
-        return  dp[i][j]
+    #     dp[i][j]=res
+    #     return  dp[i][j]
+    # def numDistinct(self, s: str, t: str) -> int:
+    #     n=len(s)
+    #     m=len(t)
+
+    #     dp=[[0]*(m+1) for _ in range(n+1)]
+
+    #     for i in range(n+1):
+    #         dp[i][0]=1
+        
+    #     for i in range(1,n+1):
+    #         for j in range(1,m+1):
+    #             if s[i-1]==t[j-1]:
+    #                 dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
+    #             else:
+    #                 dp[i][j]=dp[i-1][j]
+    #     ans=dp[n][m]
+
+    #     return ans 
+
     def numDistinct(self, s: str, t: str) -> int:
         n=len(s)
         m=len(t)
 
-        dp=[[0]*(m+1) for _ in range(n+1)]
-
-        for i in range(n+1):
-            dp[i][0]=1
-        
+        dp = [0 for i in range(m + 1)]
+        dp[0]=1
         for i in range(1,n+1):
-            for j in range(1,m+1):
+            
+            for j in range(m,0,-1):
                 if s[i-1]==t[j-1]:
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
-                else:
-                    dp[i][j]=dp[i-1][j]
-        ans=dp[n][m]
+                    dp[j]=dp[j-1]+dp[j]
+                
+            
+            
+            
+        return dp[m]
 
-        return ans 
+        
 
         
