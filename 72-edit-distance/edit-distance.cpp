08ab99211eb7d26b2d4d78e037ebcab165be2225
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-02-22 15:22:55 +0530

    Time: 4 ms (79.79%) | Memory: 13 MB (59.75%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -20,9 +20,28 @@ public:
         int n=word1.size();
         int m=word2.size();
 
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for (int i = 0; i <= n; i++) {
+        dp[i][0] = i;
+        }
+        for (int j = 0; j <= m; j++) {
+        dp[0][j] = j;
+         }
 
-        return help(word1,word2,n-1,m-1,dp);
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]=1+min({dp[i-1][j],
+                                   dp[i-1][j-1],
+                                   dp[i][j-1]        
+                                           });
+                }
+            }
+        }
+
+        return dp[n][m];
         
     }
 };
