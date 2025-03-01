ac2aa80798ebb7cdf432e6a2a931339299a78148
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-03-01 19:19:15 +0100

    Time: 51 ms (71.18%) | Memory: 14.3 MB (61.47%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -17,8 +17,21 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        if(n==0) return 0;
+        vector<int>dp(n,1);
+        int maxlen=1;
+        
+        for(int i=1;i<n;i++){
+            for(int j=0 ;j<i;j++){
+                if(nums[i]>nums[j] ){
+                    dp[i]=max(dp[i],1+dp[j]);
+                }
+            }
+            maxlen=max(maxlen,dp[i]);
+        }
+        
 
-        return help(nums,-1,0,n,dp);
+
+        return maxlen;
     }
 };
