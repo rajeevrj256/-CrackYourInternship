d139d0c849df4bfbdd3ce116b17a1d5d31879fab
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-03-14 11:46:26 +0100

    Time: 39 ms (29.92%) | Memory: 27.6 MB (28.30%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
-    int help(string & text1,int index1,string text2,int index2,vector<vector<int>>&dp){
-       
-        if(index1>=text1.size() || index2>=text2.size()) return 0;
+    int solve(string& text1,string& text2,vector<vector<int>>&dp,int i,int j){
+         
+           if(i>=text1.size() || j>=text2.size()) return 0;
 
-        if(dp[index1][index2]!=-1)return dp[index1][index2];
-        
-        if(text1[index1]==text2[index2]){
+           if(dp[i][j]!=-1) return dp[i][j];
 
-        return dp[index1][index2]=1+help(text1,index1+1,text2,index2+1,dp);
-        }else{
-            return dp[index1][index2]=max(help(text1,index1,text2,index2+1,dp),help(text1,index1+1,text2,index2,dp));
-        }
-        
+           if(text1[i]==text2[j]){
+            return dp[i][j]=1+solve(text1,text2,dp,i+1,j+1);
+           }else{
+            return dp[i][j]=max(solve(text1,text2,dp,i+1,j),solve(text1,text2,dp,i,j+1));
+           }
 
     }
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        vector<int>prev(m+1,0);
-        
-        for(int i=1;i<=n;i++){
-            vector<int>curr(m+1,0);
-            for(int j=1;j<=m;j++){
-                 if(text1[i-1]==text2[j-1]){
-                    curr[j]=1+prev[j-1];
-                 }else{
-                    curr[j]=max(curr[j-1],prev[j]);
-                 }
-            }
-            prev=curr;
-        }
-        return prev[m];
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
 
-        //return help(text1,0,text2,0,dp);
-        
+        return solve(text1,text2,dp,0,0);
     }
 };
