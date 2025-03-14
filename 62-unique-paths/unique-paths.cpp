291984f2c49d06be84e3517ba8801e7a3af9466c
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-03-14 10:00:47 +0100

    Time: 0 ms (100.00%) | Memory: 9.3 MB (56.57%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -16,9 +16,33 @@ public:
         return dp[i][j]=down+right;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        vector<vector<int>>dp(m,vector<int>(n,0));
 
-        return solve(0,0,m,n,dp);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) {
+                    dp[i][j]=1;
+                    continue;
+                }
+                int right=0;
+                int down=0;
+                if(i>0){
+
+                right=dp[i-1][j];
+                }
+                if(j>0){
+
+                down=dp[i][j-1];
+                }
+
+                dp[i][j]=right+down;
+            }
+
+        }
+
+
+
+        return dp[m-1][n-1];
         
     }
 };
