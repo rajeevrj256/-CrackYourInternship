93e0e902dfdb6a791b688d95338f51f12ddbbb42
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-03-17 07:19:41 +0100

    Time: 10 ms (29.70%) | Memory: 20.2 MB (34.89%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -4,33 +4,29 @@ public:
         vector<vector<int>>adj(numCourses);
         vector<int>indegree(numCourses,0);
 
-       
-            for(auto it:prerequisites){
-                adj[it[1]].push_back(it[0]);
-                indegree[it[0]]++;
-            }
-       
+        for(auto it:prerequisites){
+            adj[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
+
+        }
 
         queue<int>q;
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0){
-                q.push(i);
-            }
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) q.push(i);
         }
-        int course_take=0;
+
+        int course=0;
         while(!q.empty()){
-            int node=q.front();
+            int top=q.front();
             q.pop();
-            for(auto it:adj[node]){
+            course++;
+            for(auto it:adj[top]){
                 indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
-                }
+
+                if(indegree[it]==0) q.push(it);
             }
-            course_take++;
         }
 
-        return course_take==numCourses;
-
+        return course==numCourses;
     }
 };
