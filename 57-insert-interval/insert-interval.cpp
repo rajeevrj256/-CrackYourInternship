3a22e0a6eede08abf75ebf92ed9825d15fc4033f
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-03-18 08:24:10 +0100

    Time: 0 ms (100.00%) | Memory: 21.7 MB (46.03%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
-    
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int n=intervals.size();
+        int i=0;
         vector<vector<int>>ans;
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>>q;
-        for(auto it:intervals){
-            q.push(it);
-        }
+    while(i<n && intervals[i][1]<newInterval[0]){
+         ans.push_back(intervals[i]);
+         i++;
+    }
 
-        q.push(newInterval);
-        auto it=q.top();
-        q.pop();
-        int start=it[0];
-        int end=it[1];
+    while(i<n && intervals[i][0]<=newInterval[1]){
+        newInterval[0]=min(newInterval[0],intervals[i][0]);
+        newInterval[1]=max(newInterval[1],intervals[i][1]);
+        i++;
 
-        while(!q.empty()){
-            auto top=q.top();
-            q.pop();
-            int newstart=top[0];
-            int newend=top[1];
+    }
 
-            if(end>=newstart){
-                end=max(end,newend);
-            }else{
+    ans.push_back(newInterval);
 
-            ans.push_back({start,end});
-            start=newstart;
-            end=newend;
-            }
+    while(i<n){
+        ans.push_back(intervals[i]);
+        i++;
+    }
 
-        }
-        ans.push_back({start,end});
-
-        return ans;
+    return ans;
 
     }
 };
