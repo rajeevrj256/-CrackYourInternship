49ccaba410e5286dddb5e0cbda1c32fd9b73b474
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-03-19 08:31:03 +0100

    Time: 4 ms (12.63%) | Memory: 22.9 MB (56.45%) - LeetSync

---
+++ 143-reorder-list/reorder-list.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    void reorderList(ListNode* head) {
+        if (!head || !head->next) return;
+
+        // Step 1: Find the middle of the list
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Split the list into two halves
+        ListNode* secondHalf = reverse(slow->next);  // Reverse from slow->next
+        slow->next = nullptr;  // Break the list into two halves
+
+        // Step 2: Merge the two halves
+        ListNode* firstHalf = head;
+        while (secondHalf) {
+            ListNode* temp1 = firstHalf->next;
+            ListNode* temp2 = secondHalf->next;
+
+            firstHalf->next = secondHalf;
+            secondHalf->next = temp1;
+
+            firstHalf = temp1;
+            secondHalf = temp2;
+        }
+    }
+};
