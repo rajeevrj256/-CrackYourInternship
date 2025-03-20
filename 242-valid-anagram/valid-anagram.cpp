8ef48bb62eca04cdf1309161cd1511f5da9a4ab1
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-03-20 10:23:07 +0100

    Time: 0 ms (100.00%) | Memory: 10 MB (11.73%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        unordered_map<char,int>hash;
+
+        for(int i=0;i<s.size();i++){
+            hash[s[i]]++;
+            hash[t[i]]--;
+        }
+
+        for(auto it :hash){
+            if(it.second>0) return false;
+        }
+
+
+        return true;
+
+    }
+};
