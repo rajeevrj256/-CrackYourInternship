51ac065b7f2d7684ce7df959e42195f6bb91aa94
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-03-27 08:29:04 +0100

    Time: 0 ms (100.00%) | Memory: 18.7 MB (53.22%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,30 +1,44 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        int col=matrix[0][0];
         int n=matrix.size();
         int m=matrix[0].size();
-        queue<pair<int,int>>q;
-
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    q.push({i,j});
+                    matrix[i][0]=0;
+
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }else{
+                        col=0;
+                    }
                 }
             }
         }
 
-        while(!q.empty()){
-            pair<int,int>top=q.front();
-            q.pop();
-            int row=top.first;
-            int col=top.second;
-
-            for(int i=0;i<n;i++){
-                matrix[i][col]=0;
-            }
-            for(int j=0;j<m;j++){
-                matrix[row][j]=0;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0 || matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
+                }
             }
         }
+
+        if(matrix[0][0]==0){
+            for(int i=0;i<m;i++){
+                matrix[0][i]=0;
+            }
+        }
+
+        if(col==0){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
+            }
+        }
+
     }
 };
