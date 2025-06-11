bcb3f7062fb5a1a259178aec8da0e9069a32bf21
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-06-11 16:36:04 +0530

    Time: 4 ms (58.25%) | Memory: 27.5 MB (37.90%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,35 +9,29 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution { 
+class Solution {
 public:
-    unordered_map<int, int> inorder_map;  // To store value -> index for inorder traversal
-    int preorder_index = 0;  // To keep track of current node in preorder    
-    TreeNode* buildTreeHelper(const vector<int>& preorder, int inorder_left, int inorder_right) {
-        // Base case: if there are no elements to construct the tree
-        if (inorder_left > inorder_right) {
-            return nullptr;
-        }
-        // The current root is the next element in preorder traversal
-        int root_val = preorder[preorder_index++];
-        TreeNode* root = new TreeNode(root_val);
+    TreeNode* solve(vector<int>&preorder,int preStart,int preEnd,vector<int>&inorder,int inStart,int inEnd,unordered_map<int,int>&hash){
+        if(preStart>preEnd || inStart>inEnd) return NULL;
+        TreeNode* root = new TreeNode(preorder[preStart]);
+        int index=hash[preorder[preStart]];
+        int leftpart=index-inStart;
 
-        // Split the inorder array into left and right subtrees using the hashmap
-        int inorder_index = inorder_map[root_val];
-
-        // Recursively build the left and right subtrees
-        root->left = buildTreeHelper(preorder, inorder_left, inorder_index - 1);
-        root->right = buildTreeHelper(preorder, inorder_index + 1, inorder_right);
+        root->left=solve(preorder,preStart+1,preStart+leftpart,inorder,inStart,index-1,hash);
+        root->right=solve(preorder,preStart+leftpart+1,preEnd,inorder,index+1,inEnd,hash);
 
         return root;
+
+
     }
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) { 
-        // Build a hashmap to store the index of each value in inorder
-        for (int i = 0; i < inorder.size(); ++i) {
-            inorder_map[inorder[i]] = i;
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int,int>hash;
+        int n=inorder.size();
+        for(int i=0;i<n;i++){
+            hash[inorder[i]]=i;
         }
         
-        // Start building the tree using helper function
-        return buildTreeHelper(preorder, 0, inorder.size() - 1);
+        return solve(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,hash);
+
     }
 };
