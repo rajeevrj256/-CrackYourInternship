a32533f64bf3c1d89a59476fee51cafbe52fdf87
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2025-12-30 22:47:14 +0530

    Time: 130 ms (11.59%) | Memory: 55.5 MB (6.37%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
-    bool checkStar(string &p, int i) {
-        for (int j = 0; j <= i; j++) {
-            if (p[j] != '*') return false;
+ string s;
+    string p;
+    vector<vector<int>>dp;
+    bool solve(int i,int j){
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        return true;
+
+        bool ans;
+
+        if(j==p.length()){
+            return dp[i][j]= (i==s.length());
+        }else{
+             bool firstMatchChar=(i<s.length() && (s[i]==p[j] || p[j]=='?'));
+
+            if(j<p.length() && p[j]=='*'){
+           
+
+            bool not_take=solve(i,j+1);
+
+           bool take = (i < s.length()) && solve(i+1, j);
+            ans=take || not_take;
+            }else{
+              ans= firstMatchChar && solve(i+1,j+1);
+            }
+        }
+
+
+        return dp[i][j]=ans;
     }
-
-    bool help(string &p, string &s, int i, int j, vector<vector<int>> &dp) {
-        if (i < 0 && j < 0) return true;  // Both strings are exhausted
-        if (i < 0 && j >= 0) return false; // Pattern exhausted but string remains
-        if (j < 0 && i >= 0) return checkStar(p, i); // Check if remaining pattern is all '*'
-
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (p[i] == s[j] || p[i] == '?') {
-            return dp[i][j] = help(p, s, i - 1, j - 1, dp);
-        }
-        if (p[i] == '*') {
-            return dp[i][j] = help(p, s, i - 1, j, dp) || help(p, s, i, j - 1, dp);
-        }
-        return dp[i][j] = false;
-    }
-
     bool isMatch(string s, string p) {
-        int n = p.size();
-        int m = s.size();
-
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-
-        return help(p, s, n - 1, m - 1, dp);
+          this->p=p;
+        this->s=s;
+        dp.assign(s.length() + 1, vector<int>(p.length() + 1, -1));
+        return solve(0,0);
     }
 };
