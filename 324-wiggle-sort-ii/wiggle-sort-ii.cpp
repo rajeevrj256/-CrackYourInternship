eff412bf17cb61fa1799cba1089fc564d8ac08ad
RAJIV RANJAN <98413843+rajeevrj256@users.noreply.github.com>
2026-01-01 13:02:39 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (17.83%) - LeetSync

--- 324-wiggle-sort-ii/wiggle-sort-ii.cpp
+++ 324-wiggle-sort-ii/wiggle-sort-ii.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        int n = nums.size();
+        vector<int>v=nums;
+        sort(v.begin(),v.end());
+        int n=nums.size();
+        int mid=(n+1)/2;
+        int left=mid-1;
+        int right=n-1;
 
-        // 1. Find median
-        int mid = (n - 1) / 2;
-        nth_element(nums.begin(), nums.begin() + mid, nums.end());
-        int median = nums[mid];
 
-        // Virtual index mapping
-        auto idx = [&](int i) {
-            return (1 + 2 * i) % (n | 1);
-        };
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                nums[i]=v[left--];
 
-        // 2. 3-way partition
-        int i = 0, j = 0, k = n - 1;
-        while (j <= k) {
-            if (nums[idx(j)] > median) {
-                swap(nums[idx(i++)], nums[idx(j++)]);
-            } else if (nums[idx(j)] < median) {
-                swap(nums[idx(j)], nums[idx(k--)]);
-            } else {
-                j++;
+            }else{
+                nums[i]=v[right--];
             }
         }
     }
